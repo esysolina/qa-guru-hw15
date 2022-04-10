f0c45386985a927ee31160e28158f9b0dc4ac42c
esysolina <evelina.ova@gmail.com>
2022-04-10 14:47:09 +0500

    update config

--- src/test/java/guru/qa/config/Project.java
+++ src/test/java/guru/qa/config/Project.java
@@ -5,9 +5,7 @@ import org.aeonbits.owner.ConfigFactory;
 public class Project {
     public static ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
 
-//    public static boolean browser() {
-//        return !config.browser().equals("");
-//    }
+    public static boolean browser() {return !config.browser().equals(""); }
 
     public static boolean isRemoteWebDriver() {
         return !config.remoteDriverUrl().equals("");
